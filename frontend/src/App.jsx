1534628aab03e1bec00a0a4759c7393423fe8e70
kjvits <kjvita@gbox.ncf.edu.ph>
2024-12-15 14:48:53 +0800

    CRUD operation Added to Client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,9 @@ import { GET_AUTHENTICATED_USER } from './graphql/mutations/queries/user.query.j
 import { Toaster } from 'react-hot-toast';
 
 function App() {
-	const {loading, data, error} = useQuery(GET_AUTHENTICATED_USER);
+	const {loading, data} = useQuery(GET_AUTHENTICATED_USER);
 	
-	console.log ("Loading:", loading);
-	console.log("Authenticated user:", data)
-	console.log ("Error:", error);
+	
 	
 	if (loading) return null;
 
